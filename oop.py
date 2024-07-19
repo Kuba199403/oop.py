ee8cfaaa923e906ee5b421622fae281fae2fb8c0
Kuba199403 <yagmurovkuvvat@gmail.com>
2024-07-19 12:25:58 +0200

    Add files via upload

---
+++ oop.py
@@ -0,0 +1,105 @@
+"""
+Создайте игру "Магазин животных". Реализуйте базовый класс Animal (животное) с атрибутами name (имя) и price (цена),
+   а также методом sound(), который возвращает звук, издаваемый животным. От него унаследуйте классы Dog, Cat и Bird,
+   каждый из которых переопределяет метод sound() для возврата соответствующего звука для каждого типа животного.
+   Класс Shop должен иметь атрибуты animals (список доступных животных) и budget (бюджет магазина),
+   а также методы buy_animal(animal) для покупки животного и sell_animal(animal) для продажи животного.
+   Реализуйте проверки наличия достаточного бюджета у магазина для покупки и наличия животного в магазине для продажи.
+
+
+
+"""
+from __future__ import annotations
+
+"""
+) Создайте класс Library, представляющий библиотеку. 
+   Класс должен иметь атрибуты books (список книг) и members (список членов библиотеки).
+  
+  
+   Реализуйте методы add_book(book) для добавления книги в библиотеку, remove_book(book) для удаления книги из библиотеки,
+   
+   
+   add_member(member) для добавления нового члена библиотеки и remove_member(member) для удаления члена библиотеки.
+   
+   Также реализуйте метод checkout_book(book, member) для выдачи книги члену библиотеки и return_book(book, member)
+   для возврата книги в библиотеку.
+"""
+# class Library:
+#     def __init__(self, books : list[str], members: list[str]):
+#         self.books = books
+#         self.members = members
+#
+#     def add_book(self, book: str):
+#         self.book = book
+#         self.books.append(self.book)
+#
+#     def remove_book(self, book: str):
+#         self.books.pop(self.book)
+#
+#     def add_member(self, member):
+
+
+from typing import TYPE_CHECKING
+
+
+class Animal:
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
+
+    def sound(self):
+        pass
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}: {self.name}'
+
+
+class Dog(Animal):
+    def sound(self):
+        print(f'{self.name}: гаф гаф')
+
+
+class Cat(Animal):
+    def sound(self):
+        print(f'{self.name}: мяв мяв')
+
+
+class Bird(Animal):
+    def sound(self):
+        print(f'{self.name}: чур чур')
+
+
+class Shop:
+    def __init__(self, animals: list[Animal], budget: float):
+        self.animals = animals
+        self.budget = budget
+
+    def buy_animal(self, animal: Animal):
+        if animal.price <= self.budget:
+            self.animals.append(animal)
+            self.budget -= animal.price
+        else:
+            print('Недостаточный бюджет')
+
+    def sell_animal(self, animal: Animal):
+        if animal in self.animals:
+            self.animals.remove(animal)
+            self.budget += self.price
+        else:
+            print(f'Животного {animal.name} нет в магазине')
+
+    def __str__(self):
+        return f'{self.animals} | {self.budget}'
+
+
+a = Dog('alabay', 100)
+b = Cat('garaja', 50)
+c = Bird('serchejik', 10)
+
+sklep = Shop(animals=[a, b], budget=200)
+
+print(sklep)
+
+Shop.sell_animal(a)
+
+
